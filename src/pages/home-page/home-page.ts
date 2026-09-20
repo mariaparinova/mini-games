8fa96cbe85e3c0ad71c120466a4d993d874623fa
parinova <maryia.parynava@gmail.com>
2026-09-20 17:27:36 +0200

    feat: Add CTA-section to home page with styles, responsive behavior, and images

--- src/pages/home-page/home-page.ts
+++ src/pages/home-page/home-page.ts
@@ -3,12 +3,13 @@ import { createHeroSection } from './hero-section/hero-section.ts';
 import { createDivElement, createMainElement } from '../../lib/element.ts';
 import { getFooterElement } from '../common-components/footer/footer.ts';
 import { createNewGamesSection } from './new-game-section/new-game-section.ts';
+import { createCtaSection } from './cta-section/cta-section.ts';
 
 export function getHomePageElement(): HTMLElement {
   const headerElement = getHeaderElement({ activeItem: NavItem.Home });
   const footerElement = getFooterElement();
   const mainElement = createMainElement({
-    children: [createHeroSection(), createNewGamesSection()],
+    children: [createHeroSection(), createNewGamesSection(), createCtaSection()],
   });
 
   return createDivElement({
