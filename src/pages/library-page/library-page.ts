31b9e86124a047102be85df738888d86474e1f1a
parinova <maryia.parynava@gmail.com>
2026-09-26 18:41:30 +0200

    feat: Add hstyling and heading to Library page
    - Introduce `library-page.scss` with layout and typography styles.
    - Update `getLibraryPageElement` to include heading and subheading elements.

--- src/pages/library-page/library-page.ts
+++ src/pages/library-page/library-page.ts
@@ -1,11 +1,27 @@
-import { createDivElement, createSpanElement } from '../../lib/element.ts';
+import './library-page.scss';
+import { createDivElement, createHeadingElement, createSpanElement } from '../../lib/element.ts';
 
 export function getLibraryPageElement() {
-  const content = createSpanElement({
-    textContent: 'Library Page Content',
+  const heading = getLibraryHeadingElement();
+
+  return createDivElement({
+    classList: ['library-page-content'],
+    children: [heading],
+  });
+}
+
+function getLibraryHeadingElement() {
+  const heading = createHeadingElement({
+    type: 'h1',
+    textContent: 'Game Library',
+  });
+  const subheading = createSpanElement({
+    classList: ['subheading'],
+    textContent: 'Browse our collection of casual mini-games',
   });
 
   return createDivElement({
-    children: [content],
+    classList: ['heading-container'],
+    children: [heading, subheading],
   });
 }
